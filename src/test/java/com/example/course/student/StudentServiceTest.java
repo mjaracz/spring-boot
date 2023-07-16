fdf12751d48ad3ec13526ca268b55038dcf478c8
mjaracz <mjaracz@investcore.pl>
2023-07-16 10:50:04 +0200

    feat: implement unit test related to updateStudent method in StudentService

--- src/test/java/com/example/course/student/StudentServiceTest.java
+++ src/test/java/com/example/course/student/StudentServiceTest.java
@@ -1,7 +1,7 @@
 package com.example.course.student;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -14,28 +14,29 @@ import java.util.Optional;
 
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
 	@Mock
-	private StudentRepository studentRepository;
-	private StudentService underTest;
+	private StudentRepository studentRepositoryMock;
+	private StudentService studentService;
 
 	@BeforeEach
 	void setUp() {
-		underTest = new StudentService(studentRepository);
+		studentService = new StudentService(studentRepositoryMock);
+
 	}
 
 
 	@Test
 	void canGetStudents() {
-		underTest.getStudents();
-		verify(studentRepository).findAll();
+		studentService.getStudents();
+		verify(studentRepositoryMock).findAll();
 	}
 
 	@Test
@@ -45,9 +46,9 @@ class StudentServiceTest {
 			"exmaple@gmail.com",
 			LocalDate.of(2000, Month.JANUARY, 2)
 		);
-		underTest.addNewStudent(student);
+		studentService.addNewStudent(student);
 		ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
-		verify(studentRepository).save(studentArgumentCaptor.capture());
+		verify(studentRepositoryMock).save(studentArgumentCaptor.capture());
 	}
 
 	@Test
@@ -57,32 +58,32 @@ class StudentServiceTest {
 			"noCorrectEmail@example.com",
 			LocalDate.of(2000, Month.JANUARY, 4)
 		);
-		given(studentRepository.checkStudentByEmail(student.getEmail()))
+		given(studentRepositoryMock.checkStudentByEmail(student.getEmail()))
 			.willReturn(true);
-		assertThatThrownBy(() -> underTest.addNewStudent(student))
+		assertThatThrownBy(() -> studentService.addNewStudent(student))
 			.isInstanceOf(IllegalStateException.class)
 			.hasMessageContaining("email: " + student.getEmail() + " is taken");
-		verify(studentRepository, never()).save(any());
+		verify(studentRepositoryMock, never()).save(any());
 	}
 
 	@Test
 	void whenDeleteStudentAndIdNotExistThrowException() {
 		Long deletedStudentId = 2L;
-		given(studentRepository.existsById(deletedStudentId))
+		given(studentRepositoryMock.existsById(deletedStudentId))
 				.willReturn(false);
-		assertThatThrownBy(() -> underTest.deleteStudent(deletedStudentId))
+		assertThatThrownBy(() -> studentService.deleteStudent(deletedStudentId))
 				.isInstanceOf(IllegalStateException.class)
 				.hasMessageContaining("student with id " + deletedStudentId + " dose not exist");
-		verify(studentRepository, never()).deleteById(any());
+		verify(studentRepositoryMock, never()).deleteById(any());
 	}
 
+
 	@Test
-	@Disabled
 	void shouldUpdateStudent() {
-		Optional<Student> student = studentRepository.findById(2L);
+		Student mockStudentData = new Student(2L, "defaultMockName", "email@example.com", LocalDate.now());
+		when(studentRepositoryMock.findById(2L)).thenReturn(Optional.of(mockStudentData));
 
-		underTest.updateStudent(2L, "updated name", "update-me@java-advantger.pl");
-		ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
-
+		Student updatedStudent = studentService.updateStudent(2L, "updated-name", "updated@email.com");
+		assertThat(updatedStudent.equals(mockStudentData)).isTrue();
 	}
 }

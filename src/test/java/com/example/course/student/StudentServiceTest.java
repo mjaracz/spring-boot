2615f298939056b2c19d5c3e7d1ce9ce5152795c
mjaracz <mjaracz@investcore.pl>
2023-07-16 10:52:44 +0200

    fix: cleanup imports in SrudentServiceTest.java file

--- src/test/java/com/example/course/student/StudentServiceTest.java
+++ src/test/java/com/example/course/student/StudentServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.course.student;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
